b3a752db1218cbb2beedc049166ed07c12813423
Angela Loftus <angelaloftus@yahoo.com>
2021-10-31 22:50:22 -0500

    descriptive comments

--- file.js
+++
@@ -1,7 +0,0 @@
-//separate page 
-//function query for employee
-////maybe easier not to do this
-//can always pull the most useful function out later
-
-//
-//module.exports = things;
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 
+
+//prompt to ask users what they would like to do
 function userChoice() {
 inquirer.prompt([
     {
@@ -28,7 +30,8 @@ inquirer.prompt([
             ]
         }
 ]).then( answers => {
-   console.log(answers.choice);
+    console.log(answers.choice);
+   //switch statement controlling flow of application
 switch(answers.choice) {
     case "View all employees":
         viewEmployees();
@@ -68,12 +71,14 @@ switch(answers.choice) {
 })
 }
 
+//function to stop the node process and exit the application
 function exit() {
     console.log("Goodbye.");
     process.exit();
 }
 userChoice( );
 
+//function to view all of the employees in a table
 function viewEmployees() {
     const sql = `SELECT * FROM employee`;
     db.query(sql, (err,rows) => {
@@ -87,6 +92,7 @@ function viewEmployees() {
     userChoice();
 };
 
+//function to view all roles in a table
 function viewRoles() {
     const sql = `SELECT * FROM roles`;
     db.query(sql, (err,rows) => {
@@ -100,7 +106,7 @@ function viewRoles() {
     userChoice();
 };
     
-
+//function to view all departments in a table 
 function viewDepartments() {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err,rows) => {
@@ -113,7 +119,8 @@ function viewDepartments() {
     })
     userChoice();
 }
- 
+
+//function to add a department and show updated list of departments
 function addDepartment() {
     inquirer.prompt([
         {
@@ -134,7 +141,7 @@ function addDepartment() {
 })
 }
 
-//function add employee 
+//function add employee and show updated list of employees
 function addEmployee() {
     inquirer.prompt([
         {
@@ -174,6 +181,7 @@ function addEmployee() {
     });
 };
 
+//function to add a role and show updated list of roles
 function addRole() {
     inquirer.prompt([
             {
@@ -210,6 +218,7 @@ function addRole() {
         })
 }    
 
+//function to update the role of an exisiting employee and show updated list of employees
 function updateEmployeeRole() {
     inquirer.prompt([
         {
@@ -245,28 +254,3 @@ function updateEmployeeRole() {
     })
 }
 
-
-// GIVEN a command-line application that accepts user input
-
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
-
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
